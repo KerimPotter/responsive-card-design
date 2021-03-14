77ef3d1788a712e50b2f1b55db5e5ab2c0352b2b
KerimPotter <48261129+KerimPotter@users.noreply.github.com>
2021-03-14 13:33:00 +0300

    ✨ Initialize

---
+++ index.js
@@ -0,0 +1,13 @@
+const buttonContainer = document.querySelector(".buttonContainer");
+
+const button = `<button class="button">
+<a href="/random-word-selector">
+  <div class="button1-1">Responsive Card Design</div>
+  <hr>
+  <div class="button1-2">This thing...</div>
+</a>
+</button>`;
+
+for (let i = 0; i < 21; i++) {
+  buttonContainer.innerHTML += button;
+};
